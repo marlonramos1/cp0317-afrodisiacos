1cb6c30fa78b456a1ec4b8717c9195be25aba596
marlonramos1 <35278724+marlonramos1@users.noreply.github.com>
2018-02-02 17:47:49 -0600

    Archivos para ejecutar un analisis sintactico

---
+++ analizador_cprime/parser.h
@@ -0,0 +1,25 @@
+
+int error(int);
+
+void programa();
+void TIPO_ACCESO();
+void CUERPO();
+void DEC_VAR();
+void INSTRUCCION();
+void TIPO();
+void CONSTRUCTOR();
+void IF();
+void ELSE();
+void WHILE();
+void FOR();
+void SWITCH();
+void ASIGNACION();
+void PARAMETROS();
+void OBJETO();
+void CONDICION();
+void COMPARACION();
+void EXPRESION();
+void EXPNUMERICA();
+void EXPARITMETICA();
+void PRINT();
+void FORMATOW();
